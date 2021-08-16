aaa0b819a6ba04b71a4d6c9d5a0192ec9f449194
Riya Garg <riyagarg30@gmail.com>
2021-08-16 15:05:26 +0530

    Add Project
    Colour Gaze Viewer

---
+++ Assets/Scripts/Gaze.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+public class Gaze : MonoBehaviour
+{
+    List<InfoBehavior> infos = new List<InfoBehavior>();
+    void Start()
+    {
+        infos = FindObjectsOfType<InfoBehavior>().ToList();
+    }
+
+    void Update()
+    {
+        if(Physics.Raycast(transform.position, transform.forward, out RaycastHit hit))
+        {
+            GameObject go = hit.collider.gameObject;
+            if (go.CompareTag("hasInfo"))
+            {
+                OpenInfo(go.GetComponent<InfoBehavior>());
+            }
+            else
+            {
+                CloseAll();
+            }
+        }
+    }
+
+    void OpenInfo(InfoBehavior desiredInfo)
+    {
+        foreach(InfoBehavior info in infos)
+        {
+            if (info == desiredInfo)
+            {
+                info.OpenInfo();
+            }
+            else
+            {
+                info.CloseInfo();
+            }
+        }
+    }
+
+    void CloseAll()
+    {
+        foreach (InfoBehavior info in infos)
+        {
+            info.CloseInfo();
+        }
+    }
+}
---
+++ Assets/Scripts/InfoBehavior.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class InfoBehavior : MonoBehaviour
+{
+    const float SPEED = 6f;
+    [SerializeField]
+    Transform SectionInfo;
+
+    Vector3 desiredScale = Vector3.zero;
+
+
+
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        SectionInfo.localScale = Vector3.Lerp(SectionInfo.localScale, desiredScale, Time.deltaTime * SPEED);   
+    }
+
+    public void OpenInfo()
+    {
+        desiredScale = Vector3.one;
+    }
+
+    public void CloseInfo()
+    {
+        desiredScale = Vector3.zero;
+    }
+}
